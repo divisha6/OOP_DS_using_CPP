16b939c7ec43a2601c1b9649e0069605e0334eee
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2022-12-24 22:56:49 +0530

    started Week 2 content

---
+++ self-practice/cout2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "We are using std statements outside the main function" << endl;
+    return 0;
+}
+
+// resume from 1.3, 4 minutes
